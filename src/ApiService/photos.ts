1cb8b53b878e4e82c44e6eb5d51cfdebb9804bc2
natalia mohyla <mogilesha81@gmail.com>
2025-05-24 11:43:29 +0200

    fix

--- src/ApiService/photos.ts
+++ src/ApiService/photos.ts
@@ -4,29 +4,32 @@ const API_KEY = 'bpA9fAlIYjJ0wt1NKBcIv8MZGc98l3Z3aEyyo9WXAsQ';
 const BASE_URL = 'https://api.unsplash.com/search/photos';
 
 export interface UnsplashPhoto {
-  id: string;
+ id: string;
   urls: {
-    small: string;
+   small: string;
     regular: string;
-  };
+ };
   alt_description: string;
 }
 export interface UnsplashResponse {
   total: number;
   total_pages: number;
-   results: UnsplashPhoto[];
+  results: UnsplashPhoto[];
 }
-export const getPhotos= async (query: string, page: number = 1, ): Promise<UnsplashResponse> => {
-  const { data } = await axios.get<UnsplashResponse>(BASE_URL, {
-    params: {
-      query,
-      page,
-      per_page: 16,
-      client_id: API_KEY,
-      orientation: 'landscape',
-     
-    },
-  
-  });
-  return data;
+export const getPhotos = async (query: string, page: number = 1): Promise<UnsplashResponse> => {
+  try {
+    const { data } = await axios.get<UnsplashResponse>(BASE_URL, {
+      params: {
+        query,
+        page,
+        per_page: 16,
+        client_id: API_KEY,
+        orientation: 'landscape',
+      },
+    });
+    return data;
+  } catch (error) {
+    throw new Error('Something went wrong');
+  }
 };
+
